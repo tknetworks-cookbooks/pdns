f38bf73e2127ccc1b3ef7ddfc38712072fb6540f
TANABE Ken-ichi <nabeken@tknetworks.org>
2013-08-26 10:06:39 +0900

    Fix foodcritic warns

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,24 +1,17 @@
-default[:pdns][:db_host]      = "127.0.0.1"
-default[:pdns][:db_user]      = "pdns"
-default[:pdns][:db_name]      = "pdns"
-default[:pdns][:db_password]  = ""
-default[:pdns][:axfr_ips]     = []
-default[:pdns][:launch]  = "gpgsql"
-default[:pdns][:bind_inet]  = ""
-default[:pdns][:bind_inet6] = ""
-default[:pdns][:home_dir] = "/home/pdns"
+default['pdns']['db_host'] = "127.0.0.1"
+default['pdns']['db_user'] = "pdns"
+default['pdns']['db_name'] = "pdns"
+default['pdns']['db_password'] = ""
+default['pdns']['axfr_ips'] = []
+default['pdns']['launch'] = "gpgsql"
+default['pdns']['bind_inet'] = ""
+default['pdns']['bind_inet6'] = ""
+default['pdns']['home_dir'] = "/home/pdns"
 
 case platform
-when "gentoo"
-  default[:pdns][:package] = "net-dns/pdns"
-  default[:pdns][:dir] = "/etc/powerdns"
 when "debian"
-  default[:pdns][:package] = "pdns-server"
-  default[:pdns][:dir] = "/etc/powerdns"
-  default[:pdns][:deb_version] = "3.0.1-1"
-  default[:pdns][:deb_baseurl] = "http://downloads.powerdns.com/releases/deb"
-when "freebsd"
-  # pgsql使う場合はDEFAULT_PGSQL_VER=91にする
-  default[:pdns][:package] = "dns/powerdns"
-  default[:pdns][:dir] = "/usr/local/etc/pdns"
+  default['pdns']['package'] = "pdns-static"
+  default['pdns']['dir'] = "/etc/powerdns"
+  default['pdns']['deb_version'] = "3.3-1"
+  default['pdns']['deb_baseurl'] = "http://downloads.powerdns.com/releases/deb"
 end
